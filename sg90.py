835a89e8abc1ac5307fb018ade5f63c4ea8bfef2
icysamon <icysamon@gmail.com>
2024-09-22 20:29:25 +0900

    fix: thonny can not run

--- sg90.py
+++ sg90.py
@@ -2,23 +2,23 @@ from machine import Pin, PWM
 import time
 
 duty_max = 65535
-angle_min = 0.025　# -90度
-angle_middle = 0.0725 # 0度
-angle_max = 0.12 # 90度
+deg_min = 0.025 # -90
+deg_middle = 0.0725 # 0
+deg_max = 0.12 # 90
 
-# 初期化
+# init
 def init(pin_pwm = 0, freq = 50):
     global servo
     servo = PWM(Pin(pin_pwm))
     servo.freq(freq)
 
-# テスト
+# example function
 def example(interval = 3):
-    servo.duty_u16(round(duty_max * angle_min))
+    servo.duty_u16(round(duty_max * deg_min))
     time.sleep(interval)
         
-    servo.duty_u16(round(duty_max * angle_middle))
+    servo.duty_u16(round(duty_max * deg_middle))
     time.sleep(interval)
         
-    servo.duty_u16(round(duty_max * angle_max))
+    servo.duty_u16(round(duty_max * deg_max))
     time.sleep(interval)
